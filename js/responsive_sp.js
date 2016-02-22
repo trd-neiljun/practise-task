3708f9e6a82d076f0db4df13e82c913ccab276b5
ralph <trd.ralphmirasol@gmail.com>
2016-02-22 17:22:26 +0800

    fix menubar

---
+++ js/responsive_sp.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('#g01_top').click(function(){
+		if($('#g02').css('display') == 'none'){
+			$('#gnv li').show();
+		}
+		else{
+			$('#gnv li').css('display','none');
+			$('#g01_top').show();
+			
+		}
+	})
+});
